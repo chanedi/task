62acc16e4f0d66741336def280f93358fe2e5dac
chanedi <chanedi@163.com>
2022-12-14 15:22:33 +0800

    自动创建release file

--- src/main/java/com/lufax/task/toolwindow/actions/GotoLocalTaskAction.java
+++ src/main/java/com/lufax/task/toolwindow/actions/GotoLocalTaskAction.java
@@ -18,8 +18,12 @@ import java.util.Map;
 public class GotoLocalTaskAction extends TaskItemAction {
 
     private static Map<String, Task> localTaskMap = new HashMap<>();
+    private static ThreadLocal<Task> taskThreadLocal = new ThreadLocal<>();
 
     public static Task getRepositoryTask(LocalTask localTask) {
+        if (localTaskMap.get(localTask.getId()) == null) {
+            localTaskMap.put(localTask.getId(), taskThreadLocal.get());
+        }
         return localTaskMap.get(localTask.getId());
     }
 
@@ -47,6 +51,7 @@ public class GotoLocalTaskAction extends TaskItemAction {
         JBPopup hint = DocumentationManager.getInstance(project).getDocInfoHint();
         if (hint != null) hint.cancel();
         ApplicationManager.getApplication().invokeLater(() -> {
+            taskThreadLocal.set(task);
             OpenTaskDialog openTaskDialog = new OpenTaskDialog(project, task);
             try {
                 Field field = OpenTaskDialog.class.getDeclaredField("myTask");
@@ -55,6 +60,8 @@ public class GotoLocalTaskAction extends TaskItemAction {
                 localTaskMap.put(localTask.getId(), task);
             } catch (Exception e) {
                 throw new RuntimeException(e);
+            } finally {
+                taskThreadLocal.remove();
             }
             openTaskDialog.show();
         });
