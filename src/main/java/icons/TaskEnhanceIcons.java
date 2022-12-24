b49b1ce7cc4af8ea5665eab3325d65c3734e0e47
chanedi <chanedi@163.com>
2022-12-24 10:00:33 +0800

    修复图标

--- src/main/java/icons/TaskEnhanceIcons.java
+++ src/main/java/icons/TaskEnhanceIcons.java
@@ -7,10 +7,10 @@ import javax.swing.*;
 
 public final class TaskEnhanceIcons {
 
-    /** 13x13 */ public static final @NotNull Icon ToolWindowTasks = IconLoader.getIcon("icons/tasks.svg", TaskEnhanceIcons.class);
+    /** 13x13 */ public static final @NotNull Icon ToolWindowTasks = IconLoader.getIcon("/icons/tasks.svg", TaskEnhanceIcons.class);
 
-    /** 16x16 */ public static final @NotNull Icon Super = IconLoader.getIcon("icons/super.svg", TaskEnhanceIcons.class);
-    /** 16x16 */ public static final @NotNull Icon Wizard = IconLoader.getIcon("icons/wizard.svg", TaskEnhanceIcons.class);
-    /** 16x16 */ public static final @NotNull Icon IRelease = IconLoader.getIcon("icons/irelease.svg", TaskEnhanceIcons.class);
+    /** 16x16 */ public static final @NotNull Icon Super = IconLoader.getIcon("/icons/super.svg", TaskEnhanceIcons.class);
+    /** 16x16 */ public static final @NotNull Icon Wizard = IconLoader.getIcon("/icons/wizard.svg", TaskEnhanceIcons.class);
+    /** 16x16 */ public static final @NotNull Icon IRelease = IconLoader.getIcon("/icons/irelease.svg", TaskEnhanceIcons.class);
 
 }
